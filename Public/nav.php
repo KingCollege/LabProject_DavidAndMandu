83fbbb046594efbca11680f9ef3589d3a68aa9de
DavidMahgerefteh <37069476+DavidMahgerefteh@users.noreply.github.com>
2018-12-11 01:30:43 +0000

    Create nav

---
+++ Public/nav.php
@@ -0,0 +1,23 @@
+<nav class ="menu" style ="text-align: center">
+    <a href="<?php echo LogOut;?>" id= "feature" class ="btn btn-default">
+    <h6 class = "menubar" style="color:red"> Log Out</h6> 
+     </a>
+    <a href = "rentals.php" id= "rental" class ="btn btn-default">
+    <h6 class = "menubar" > Add Rental / Rentals </h6> 
+    </a>
+    <a href = "stock.php" id= "game" class ="btn btn-default">
+    <h6 class = "menubar" > Add Game / Games </h6> 
+    </a>
+    <a href = "members.php" id= "member" class ="btn btn-default">
+    <h6 class = "menubar" >Members </h6>
+    </a>
+    <a href = "rules.php" id= "rule" class ="btn btn-default">
+    <h6 class = "menubar" > Rules </h6>
+    </a>
+    <a href = "ban.php" id= "ban" class ="btn btn-default">
+    <h6 class = "menubar" >Ban Members </h6>
+    </a>
+    <a href = "reports.php" id= "report" class ="btn btn-default">
+    <h6 class = "menubar" > Reports </h6>
+    </a>
+</nav>
